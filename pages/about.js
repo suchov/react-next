eeddcd73aad89e38ac34ab4b73f791eb13a18a4a
suchov <artem.suchov@gmail.com>
2019-06-13 00:51:54 +0300

    initial and config the materialUI

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+const About = ({ title = "Hello from about" }) => (
+  <div>
+    <h2>{title}</h2>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = ({ title = "Hello from next.js" }) => (
+  <div>
+    <h2>{title}</h2>
+  </div>
+);
+
+export default Index;
